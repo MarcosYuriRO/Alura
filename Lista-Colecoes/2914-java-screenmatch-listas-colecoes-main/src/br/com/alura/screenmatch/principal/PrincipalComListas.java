6fe4598ef3bb19a8e93383bbf76fea9dd6d8ac3a
Marcos Yuri <marcos.yuriro@gmail.com>
2025-09-01 11:47:24 -0300

    Add files via upload

---
+++ Lista-Colecoes/2914-java-screenmatch-listas-colecoes-main/src/br/com/alura/screenmatch/principal/PrincipalComListas.java
@@ -0,0 +1,35 @@
+package br.com.alura.screenmatch.principal;
+
+import br.com.alura.screenmatch.modelos.Filme;
+import br.com.alura.screenmatch.modelos.Serie;
+import br.com.alura.screenmatch.modelos.Titulo;
+
+import java.util.ArrayList;
+
+public class PrincipalComListas {
+    public static void main(String[] args) {
+        Filme meuFilme = new Filme("O poderoso chefão", 1970);
+        meuFilme.avalia(8);
+        Filme outroFilme = new Filme("Avatar", 2023);
+        outroFilme.avalia(6);
+        var filmeDoPaulo = new Filme("Dogville", 2003);
+        filmeDoPaulo.avalia(10);
+        Serie lost = new Serie("Lost", 2000);
+
+        ArrayList<Titulo> lista = new ArrayList<>();
+        lista.add(filmeDoPaulo);
+        lista.add(meuFilme);
+        lista.add(outroFilme);
+        lista.add(lost);
+        for (Titulo item : lista) {
+                //Cada item/Título que está dentro da lista...
+            System.out.println(item.getNome());
+                //exibirá o nome de cada filme na ordem que foram adicionados, até acabar a lista;
+
+            if(item instanceof Filme filme) {
+                //Se item instancia de Filme, o coloca dentro da variável filme;
+                System.out.println(filme.getClassificacao());
+            }
+        }
+    }
+}
