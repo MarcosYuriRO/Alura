4f356ee66e8eda07ab280be3e203487a1ad2ddc6
Marcos Yuri <marcos.yuriro@gmail.com>
2025-09-03 21:30:12 -0300

    Add files via upload

---
+++ Lista-Colecoes/DesafioTopico4/src/Main.java
@@ -0,0 +1,44 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        //Desafio 1:
+        /*List<Integer> numeros = new ArrayList<>();
+        numeros.add(3);
+        numeros.add(1);
+        numeros.add(2);
+
+        System.out.println(numeros);
+        Collections.sort(numeros);
+        System.out.println(numeros);*/
+
+
+        //Desafio 2/3:
+        /*Titulo titulo1 = new Titulo();
+        titulo1.setNome("O Náufrago");
+        Titulo titulo2 = new Titulo();
+        titulo2.setNome("Kraven");
+        Titulo titulo3 = new Titulo();
+        titulo3.setNome("Alvin e os Esquilos");
+
+        List<Titulo> titulos = new ArrayList<>();
+        titulos.add(titulo1);
+        titulos.add(titulo2);
+        titulos.add(titulo3);
+        System.out.println(titulos);
+        Collections.sort(titulos);
+        System.out.println(titulos);*/
+
+
+        //Desafio 4/5:
+        /*List<String> lista = new LinkedList<>();
+        //List<String> lista = new ArrayList<>();
+        lista.add("A");
+        lista.add("B");
+        lista.add("C");
+        System.out.println(lista);*/
+    }
+}
