bf489fe2d7a35ab7c169ad7a67a4b8112528f182
Marcos <marcos.yuriro@gmail.com>
2025-10-07 11:43:36 -0300

    ScreenMatch Topico 3

--- API/ProjetoMain/src/br/com/alura/screenmatch/modelos/Titulo.java
+++ API/ProjetoMain/src/br/com/alura/screenmatch/modelos/Titulo.java
@@ -3,9 +3,7 @@ package br.com.alura.screenmatch.modelos;
 import com.google.gson.annotations.SerializedName;
 
 public class Titulo implements Comparable<Titulo> {
-    @SerializedName("Title")
     private String nome;
-    @SerializedName("Year")
     private int anoDeLancamento;
     private boolean incluidoNoPlano;
     private double somaDasAvaliacoes;
@@ -19,6 +17,11 @@ public class Titulo implements Comparable<Titulo> {
 
     public Titulo(TituloOmdb meuTituloOmdb) {
         this.nome = meuTituloOmdb.title();
+
+        if (meuTituloOmdb.year().length() > 4){
+            throw new ErroDeConversaoDeAnoException("Não consegui converter o ano pois tem mais " +
+                    "de 04 caracteres");
+        }
         this.anoDeLancamento = Integer.valueOf(meuTituloOmdb.year());
         this.duracaoEmMinutos = Integer.valueOf(meuTituloOmdb.runtime().substring(0,2));
     }
