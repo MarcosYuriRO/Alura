418b7bf9755f9939aa5fd0256f4847ac42edb0df
Marcos Yuri <marcos.yuriro@gmail.com>
2025-10-08 11:46:01 -0300

    Add files via upload

--- Spring/ScreenMatch/src/main/java/br/com/alura/ScreenMatch/ScreenMatchApplication.java
+++ Spring/ScreenMatch/src/main/java/br/com/alura/ScreenMatch/ScreenMatchApplication.java
@@ -1,13 +1,23 @@
 package br.com.alura.ScreenMatch;
 
+import br.com.alura.ScreenMatch.service.ConsumoAPI;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ScreenMatchApplication {
+public class ScreenMatchApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ScreenMatchApplication.class, args);
+
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		ConsumoAPI consumoAPI = new ConsumoAPI();
+		String json = consumoAPI.obterDados("https://www.omdbapi.com/?t=gilmore+girls&apikey=d1953970");
+		System.out.println(json);
+
+	}
 }
