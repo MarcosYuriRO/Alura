1a9567d02f8b5689faf47631317890aad87d4fd4
Marcos Yuri <marcos.yuriro@gmail.com>
2025-10-10 09:29:47 -0300

    Add files via upload

--- Spring/ScreenMatch/src/main/java/br/com/alura/ScreenMatch/ScreenMatchApplication.java
+++ Spring/ScreenMatch/src/main/java/br/com/alura/ScreenMatch/ScreenMatchApplication.java
@@ -1,13 +1,30 @@
 package br.com.alura.ScreenMatch;
 
+import br.com.alura.ScreenMatch.model.DadosEpisodio;
+import br.com.alura.ScreenMatch.model.DadosSerie;
+import br.com.alura.ScreenMatch.service.ConsumoAPI;
+import br.com.alura.ScreenMatch.service.ConverteDados;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ScreenMatchApplication {
+public class ScreenMatchApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ScreenMatchApplication.class, args);
 	}
 
+
+	@Override
+	public void run(String... args) throws Exception {
+		ConsumoAPI consumoAPI = new ConsumoAPI();
+		String json = consumoAPI.obterDados("https://www.omdbapi.com/?t=gilmore+girls&apikey=d1953970");
+		System.out.println(json);
+		ConverteDados conversor = new ConverteDados();
+		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
+		System.out.println(dados);
+		json = consumoAPI.obterDados("");
+		DadosEpisodio dadosEpisodio = conversor.obterDados(json, DadosEpisodio.class);
+	}
 }
