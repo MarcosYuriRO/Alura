c94babf50c61123b89142f84452014d6441b34e0
Marcos Yuri <marcos.yuriro@gmail.com>
2025-10-14 11:34:34 -0300

    Add files via upload

--- Spring/ScreenMatch/src/main/java/br/com/alura/ScreenMatch/principal/Principal.java
+++ Spring/ScreenMatch/src/main/java/br/com/alura/ScreenMatch/principal/Principal.java
@@ -1,5 +1,6 @@
 package br.com.alura.ScreenMatch.principal;
 
+import br.com.alura.ScreenMatch.model.DadosEpisodio;
 import br.com.alura.ScreenMatch.model.DadosSerie;
 import br.com.alura.ScreenMatch.model.DadosTemporada;
 import br.com.alura.ScreenMatch.service.ConsumoAPI;
@@ -34,5 +35,22 @@ public class Principal {
         }
         //para cada item/temporada na lista "temporadas", exibe o json dela
         temporadas.forEach(System.out::println);
+
+        /*for (int i = 0; i < dados.totalTemporadas(); i++){
+            //Pega uma temporada por vez e pega seus episódios
+            List<DadosEpisodio> episodiosTemporada = temporadas.get(i).episodios();
+            //Faz uma lista de episodios de uma determinada temporada e os adiciona em ordem
+            for (int j = 0; j < episodiosTemporada.size(); j++){
+                //Exibe cada titulo dos episodios de cada temporada em ordem, até acabar a lista de episódios
+                System.out.println(episodiosTemporada.get(j).titulo());
+            }
+        }*/
+
+        //Faz o mesmo que o código comentado (t = temporadas, e = episodiosTemporada
+        //Para cada temporada/t, pega seus episódios
+        //Para cada episódio/e de uma temporada, exibe seu título
+        temporadas.forEach(t -> t.episodios().forEach(e -> System.out.println(e.titulo())));
+        //Para cada temporada, exibe ela
+        //temporadas.forEach(System.out::println);
     }
 }
