40ac103616318406b37bf9bfe1ba3f01cce6327a
Marcos Yuri <marcos.yuriro@gmail.com>
2025-12-08 21:39:52 -0300

    Add files via upload

--- Praticando-Java/src/StringsRegex/Desafio03/Main03.java
+++ Praticando-Java/src/StringsRegex/Desafio03/Main03.java
@@ -1,7 +1,19 @@
 package StringsRegex.Desafio03;
 
+import java.util.Scanner;
+
 public class Main03 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite o nome do arquivo: ");
+        String fileName = scanner.nextLine();
+
+        // \\..+$: \\. é a string com qualquer caractere. 
+        // O ponto depois, significa que ele pegará a parte da string a partir de o ponto (.), até o resto final da string ($).
+        // + pega um ou mais caracteres antes do final da string.
+        // * pega 0 ou mais caracteres antes do final da string.
+        System.out.println("Nome do arquivo sem extensão: " + fileName.replaceAll("\\..+$", ""));
         
     }
 }
